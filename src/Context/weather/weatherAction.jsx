2aed6911dab4e2347f77f0d837e679f107a8fa52
Anujarwal <anujarwal1999@gmail.com>
2024-05-15 16:43:08 +0530

    weatherAction change

--- src/Context/weather/weatherAction.jsx
+++ src/Context/weather/weatherAction.jsx
@@ -1,10 +1,13 @@
 export const fetchWeather = async (topic) => {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=2d374ae7f534467daf660256240204 &q=${topic}&aqi=yes`
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=2d374ae7f534467daf660256240204 &q=${topic}&aqi=yes`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error)
+  }
 };
 
-
-export default fetchWeather
+export default fetchWeather;
